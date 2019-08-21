d178476b8568a69aabb58d755611aa509e4a0ee3
Neeraj Gangrade Porwal <neerajgp4@gmail.com>
2019-08-21 15:54:46 +1000

    Added buttons Added images Added code

--- app/src/main/java/student/uts/edu/au/baristabrosapp/LoginActivity.java
+++ app/src/main/java/student/uts/edu/au/baristabrosapp/LoginActivity.java
@@ -2,12 +2,31 @@ package student.uts.edu.au.baristabrosapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText Email;
+    private EditText Password;
+    private Button Login;
+    private Button NewUser;
+    private Button ForgotPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Email = (EditText) findViewById(R.id.etEmail);
+        Password = (EditText) findViewById(R.id.etPassword);
+        Login = (Button) findViewById(R.id.btnLogin);
+        NewUser = (Button) findViewById(R.id.btnSignUp);
+        ForgotPassword = (Button) findViewById(R.id.btnResetPassword);
+    }
+
+    private void validate (String userName, String userPassword){
+        if((userName == "Admin") && (userPassword == "12"));
     }
 }
