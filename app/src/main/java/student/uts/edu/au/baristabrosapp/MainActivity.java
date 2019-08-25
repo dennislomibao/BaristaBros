49e046746e5ffd4e46822a2fe9da27ce0b17b6aa
Neeraj Gangrade Porwal <neerajgp4@gmail.com>
2019-08-25 17:27:55 +1000

    Added a menu inflater Added a logout method

--- app/src/main/java/student/uts/edu/au/baristabrosapp/MainActivity.java
+++ app/src/main/java/student/uts/edu/au/baristabrosapp/MainActivity.java
@@ -1,15 +1,63 @@
 package student.uts.edu.au.baristabrosapp;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        firebaseAuth = firebaseAuth.getInstance();
+
+        logout = (Button) findViewById(R.id.btnLogout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Logout();
+            }
+        });
+
+
+    }
+
+    private void Logout (){
+        firebaseAuth.signOut();
+        finish();
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logoutMenu:{
+                Logout();
+
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
