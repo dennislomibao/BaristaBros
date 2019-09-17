d87cbb77afe106f1454ba1197ea4b9b9498723d6
Neeraj <neerajgp4@gmail.com>
2019-09-17 17:35:00 +1000

    Added a working payment method

--- app/src/main/java/student/uts/edu/au/baristabrosapp/PaymentMethod.java
+++ app/src/main/java/student/uts/edu/au/baristabrosapp/PaymentMethod.java
@@ -1,30 +1,69 @@
 package student.uts.edu.au.baristabrosapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.braintreepayments.cardform.view.CardForm;
 
-import org.w3c.dom.Text;
 
 public class PaymentMethod extends AppCompatActivity {
+
+    CardForm cardForm;
+    Button buy;
+    AlertDialog.Builder alertBuilder;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_method);
 
-        CardForm cardForm = (CardForm) findViewById(R.id.credit_card);
-        //TextView txtDes = (TextView) findViewById(R.id.payment_ammount);
-        Button btnPay = (Button)findViewById(R.id.pay_now);
+        cardForm = findViewById(R.id.credit_card);
+        buy = findViewById(R.id.pay_now);
+        cardForm.cardRequired(true)
+                .expirationRequired(true)
+                .cvvRequired(true)
+                .postalCodeRequired(true)
+                .mobileNumberExplanation("SMS")
+                .mobileNumberRequired(true)
+                .setup(PaymentMethod.this);
+        cardForm.getCvvEditText().setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_VARIATION_PASSWORD);
+        buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cardForm.isValid()){
+                    alertBuilder = new AlertDialog.Builder(PaymentMethod.this);
+                    alertBuilder.setTitle("Confirm");
+                    alertBuilder.setMessage("Card Number" + cardForm.getCardNumber() + "\n" +
+                            "Card expiry date: " + cardForm.getExpirationDateEditText().getText().toString() + "\n" +
+                            "Card CVV: " + cardForm.getCvv() + "\n" +
+                            "Postal Code: " + cardForm.getPostalCode() + "\n" +
+                            "Phone number" + cardForm.getMobileNumber());
+                    alertBuilder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
 
-       // txtDes.setText("$1999");
-       // btnPay.setText(String.format("Payer %s", txtDes.getText()));
+                            Toast.makeText(PaymentMethod.this, "thank you for oyur purchase", Toast.LENGTH_LONG).show();
+                        }
+                    });
 
-        //CardForm
+
+
+
+
+
+                }
+            }
+        });
+
+
 
 
     }
